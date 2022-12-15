b36447afab3e155d676ba24231eccc2d3fed20c8
Juleian Renteria <Jprenteria02@gmail.com>
2022-12-15 01:36:22 -0800

    adding styles to rest-rant

--- views/default.jsx
+++ views/default.jsx
@@ -5,6 +5,7 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
+                https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js
                 <link rel="stylesheet" href="/css/style.css" />
             </head>
             <body>
