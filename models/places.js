3b087f7cd04226b286eaf589d812c5ea3b997fb7
Juleian Renteria <Jprenteria02@gmail.com>
2022-12-17 06:32:36 -0800

    finished part 8

--- models/places.js
+++ models/places.js
@@ -1,13 +1,13 @@
-module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/public/images/nick-karvounis-Ciqxn7FE4vE-unsplash.jpg/250/250'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/public/images/beatris-m-5RsnvSk1-0U-unsplash.jpg/250/250'
-  }]
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
+
