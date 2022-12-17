e7810866024ff943c930de6a04a8eb0eacdee796
Juleian Renteria <Jprenteria02@gmail.com>
2022-12-17 06:12:26 -0800

    Added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -1,20 +1,32 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
-router.post("/", (req, res) => {
-  if (!req.body.pic) {
-	  req.body.pic = 'http://placekitten.com/400/400'
-	}
-	if (!req.body.city) {
-	  req.body.city = 'Anytown'
-	}
-	if (!req.body.state) {
-	  req.body.state = 'USA'
-	}
-  places[id] =(req.body)
-  res.redirect(`/places/${id}`)
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
   }
-)
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
+})
 router.get('/', (req, res) => {      
   res.render('places/index', {places})
 }) 
@@ -33,19 +45,6 @@ router.get('/:id', (req, res) => {
   else {
     res.render('places/show', {place: places[id], id})
   } 
-})
-
-router.put("/:id/", (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render("error404")
-  } else if (!places[id]) {
-    res.render("error404")
-  } else {
-    
-    places[id] = req.body
-    res.redirect(`/places/${id}`)
-  }
 })
 
 router.delete('/:id', (req, res) => {
